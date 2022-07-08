b61aa2b5c5771a12b83b7c1f5c869e9c385776d5
‏‏‏‏Matan‏ ‏‏‏‏ ‏‏ ‏ <drms12@gmail.com>
2022-07-08 16:25:05 +0300

    Grid Products View

--- client/src/components/ProductGridView/ProductGridView.jsx
+++ client/src/components/ProductGridView/ProductGridView.jsx
@@ -1,6 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../Rating/Rating";
+import { IconContext } from "react-icons";
+import { BsFillCaretRightFill } from "react-icons/bs";
 import "./ProductGridView.css";
 
-export default function ProductGridView() {
-  return <div>ProductGridView</div>;
+export default function ProductGridView({
+  name,
+  brand,
+  id,
+  rating,
+  numReviews,
+  image,
+  price,
+}) {
+  return (
+    <div className="product-grid-view-container">
+      <div className="product-grid-view-image">
+        <Link to={`/products/${id}`}>
+          <img src={`${image}`} alt={`${name}`} />
+        </Link>
+      </div>
+      <div className="product-grid-view-branding">
+        <Rating rating={rating} numReviews={numReviews} />
+        <div className="product-list-view-brand">{brand}</div>
+      </div>
+      <Link to={`/products/${id}`}>
+        <div className="product-grid-view-name">{name}</div>
+      </Link>
+      <div className="product-grid-view-price">${price.toLocaleString()}</div>
+      <div style={{ fontSize: 12, weight: 400, marginTop: 5 }}>
+        Free Shipping
+      </div>
+      <IconContext.Provider value={{ size: 10 }}>
+        <button className="product-list-view-cart-btn">
+          <span>Add to Cart</span>
+          <span>
+            <BsFillCaretRightFill />
+          </span>
+        </button>
+      </IconContext.Provider>
+    </div>
+  );
 }
